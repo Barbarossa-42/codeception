bb32057d5b813256c8b457a81c10d48cb505cf8e
Lukáš Prudil <lukas.prudil@gmail.com>
2020-03-08 15:27:33 +0100

    Add support for parameters (custom extension)
    Useful when is working wit environment variables...

--- src/Module/NetteDIModule.php
+++ src/Module/NetteDIModule.php
@@ -19,6 +19,9 @@ class NetteDIModule extends Module
 
 	/** @var callable[] function(Container $container): void; */
 	public $onCreateContainer = [];
+	
+	/** @var array */
+	public $parameters = [];
 
 	/** @var mixed[] */
 	protected $config = [
@@ -128,11 +131,13 @@ class NetteDIModule extends Module
 			FileSystem::createDir($logDir);
 			$configurator->enableDebugger($logDir);
 		}
-
-		$configurator->addParameters([
+		
+		$parameters = array_merge($this->parameters, [
 			'appDir' => $this->path . ($this->config['appDir'] !== null ? '/' . $this->config['appDir'] : ''),
 			'wwwDir' => $this->path . ($this->config['wwwDir'] !== null ? '/' . $this->config['wwwDir'] : ''),
 		]);
+
+		$configurator->addParameters($parameters);
 
 		$this->clearTempDir();
 		$tempDir = $this->getTempDir();
